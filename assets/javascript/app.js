10f9a01528f5976633da66b2b423877380b1a286
Brucimus <Brucimus@gmail.com>
2018-07-08 19:34:26 -0700

    create html, reset, and initial js

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+// Initialize Variables
+var trainName = "";
+var destination = "";
+var initialTime = "";
+var runFrequency = 0;
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDUSscaxURnRLcYCMYA38KklKcL8icp9qs",
+    authDomain: "brucimus-testing-1.firebaseapp.com",
+    databaseURL: "https://brucimus-testing-1.firebaseio.com",
+    projectId: "brucimus-testing-1",
+    storageBucket: "brucimus-testing-1.appspot.com",
+    messagingSenderId: "978947948224"
+};
+
+firebase.initializeApp(config);
+
+// Capture Button Click
+$("#formSubmit").on("click", function(event) {
+    event.preventDefault();
+
+    // Grabbed values from text boxes
+    trainName = $("#inputTrainName").val().trim();
+    destination = $("#inputDestination").val().trim();
+    initialTime = $("#inputInitTimet").val().trim();
+    runFrequency = $("#inputFrequency").val().trim();
+
+    // Code for handling the push
+    database.ref().push({
+        name: name,
+        email: email,
+        age: age,
+        comment: comment,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
